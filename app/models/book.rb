d74acb1a85984456fbd96193a8118f458e6a3e2a
VladKalorad <vladchernav210@gmail.com>
2022-11-25 00:49:10 +0300

    L-006/add book validates

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :name, presence: { message: 'Book added' }
+  validates :name, uniqueness: { message: 'unique' }
+  validates :year, numericality: { only_integer: true }
 end
